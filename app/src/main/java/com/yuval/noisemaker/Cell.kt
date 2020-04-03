90139ba720c829baab6bd8db0bb011412876495b
Yuval Aisemberg <aisembergyuval@gmail.com>
2020-04-02 18:28:42 -0700

    la

--- app/src/main/java/com/yuval/noisemaker/Cell.kt
+++ app/src/main/java/com/yuval/noisemaker/Cell.kt
@@ -9,12 +9,30 @@ class Cell @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
 
+    private var state: CellState = CellState.Off
+
     init {
         inflate(context, R.layout.cell, this)
         setOnClickListener { onClick() }
     }
 
     private fun onClick() {
-        findViewById<View>(R.id.square).setBackgroundColor(0xffff0000.toInt())
+        state = if (state == CellState.Off) CellState.On else CellState.Off
+        
+        refreshState()
+    }
+
+    private fun refreshState() {
+        val color = when (state) {
+            CellState.On -> 0xffff0000.toInt()
+            CellState.Off -> 0xffeeeeee.toInt()
+        }
+        findViewById<View>(R.id.square).setBackgroundColor(color)
+
+    }
+
+    enum class CellState {
+        On,
+        Off
     }
 }
